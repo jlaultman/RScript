2d81de0f4f4aedd4c8176d89b806b09673626178
jlaultman <joeyaultman@gmail.com>
2016-02-29 20:17:53 -0500

    Added files via upload

---
+++ VXX.Indicators.R
@@ -0,0 +1,81 @@
+setSymbolLookup(VIX='yahoo',XVX='yahoo', VXX='yahoo') 
+getSymbols(c("^VIX","^VXV","VXX"))
+
+VXX <- as.xts(read.zoo("T:/DERIV/Aultman/Projects/Data Sets/VXX OHLC 2009-2016.csv",
+                       sep=',', 
+                       tz='',   
+                       header=T,
+                       format='%m/%d/%Y %H:%M:%S'))
+
+Ratio<-Cl(VIX)/Cl(VXV)*100
+colnames(Ratio)[1] <- "Ratio"
+Ratio<-Ratio["2010/"]
+
+Data<-VXX["2010/"]
+plot(VXX)
+
+CCI13<-CCI(Data[,2:4],n=13)
+
+uEMA15<-EMA(Cl(Data),n = 15, wilder = FALSE) + (EMA(Cl(Data),n = 15, wilder = FALSE) *0.03)
+uEMA15c<-(Cl(Data)-uEMA15)/Cl(Data)*100
+
+lEMA15<-EMA(Cl(Data),n = 15, wilder = FALSE) - (EMA(Cl(Data),n = 15, wilder = FALSE) *0.03)
+lEMA15c<-(Cl(Data)-lEMA15)/Cl(Data)*100
+
+Indicators<-data.frame(Ratio,CCI13,uEMA15c,lEMA15c)
+colnames(Indicators)[2] <- "CCI13"
+colnames(Indicators)[3] <- "uEMA15c"
+colnames(Indicators)[4] <- "lEMA15c"
+Indicators<-round(Indicators,2)
+Indicators<-Indicators[-nrow(Data),]
+
+Price<-Cl(Data)-Op(Data)
+Class<-ifelse(Price > 0 ,"UP","DOWN")
+Class<-Class[-1]
+
+DataSet<-data.frame(Indicators,Class)
+DataSet<-na.omit(DataSet)
+colnames(DataSet)[5] <- "Class"
+
+Training<-DataSet[1:1000,];Test<-DataSet[1001:1300,];Validation<-DataSet[1301:1525,]
+
+NB<-naiveBayes(Class ~ Ratio + CCI13 + lEMA15c + uEMA15c, data=Training)
+table(predict(NB,Test,type="class"),Test[,5],dnn=list('predicted','actual'))
+
+TrainingPredictions<-predict(NB,Training,type="class")
+TrainingCorrect<-ifelse(TrainingPredictions==Training[,5],"Correct","Incorrect")
+TrainingData<-data.frame(Training,TrainingPredictions,TrainingCorrect)
+
+ggplot(TrainingData,aes(x=Ratio,fill=TrainingPredictions))+geom_histogram(binwidth=5,position="fill")+labs(title="Training Predictions: Ratio", x = "VIX/VXV Ratio", y= "Up/Down Ratio",fill="Predictions")+scale_fill_manual(values=c("#FF6737","#00B204"))
+ggplot(TrainingData,aes(x=CCI13,fill=TrainingPredictions))+geom_histogram(binwidth=15,position="fill")+labs(title="Training Predictions: CCI", x = "13-Period CCI", y= "Up/Down Ratio",fill="Predictions")+scale_fill_manual(values=c("#FF6737","#00B204"))
+ggplot(TrainingData,aes(x=uEMA15c,fill=TrainingPredictions))+geom_histogram(binwidth=1,position="fill")+labs(title="Training Predictions: MAEupper", x = "15-Period MAEupper", y= "Up/Down Ratio",fill="Predictions")+scale_fill_manual(values=c("#FF6737","#00B204"))
+ggplot(TrainingData,aes(x=lEMA15c,fill=TrainingPredictions))+geom_histogram(binwidth=1,position="fill")+labs(title="Training Predictions: MAElower", x = "15-Period MAElower", y= "Up/Down Ratio",fill="Predictions")+scale_fill_manual(values=c("#FF6737","#00B204"))
+
+
+TestPredictions<-predict(NB,Test,type="class")
+TestCorrect<-ifelse(TestPredictions==Test[,5],"Correct","Incorrect")
+TestData<-data.frame(Test,TestPredictions,TestCorrect)
+
+ggplot(TestData,aes(x=Ratio,fill=TestCorrect))+geom_histogram(binwidth=5,position="fill")+labs(title="Test Accuracy: Ratio", x = "VIX/VXV Ratio", y= "Correct/Incorrect Ratio",fill="Accuracy")+scale_fill_manual(values=c("#0066FF","#FF4747"))
+ggplot(TestData,aes(x=CCI13,fill=TestCorrect))+geom_histogram(binwidth=15,position="fill")+labs(title="Test Accuracy: CCI", x = "13-Period CCI", y= "Correct/Incorrect Ratio",fill="Accuracy")+scale_fill_manual(values=c("#0066FF","#FF4747"))
+ggplot(TestData,aes(x=uEMA15c,fill=TestCorrect))+geom_histogram(binwidth=1,position="fill")+labs(title="Test Accuracy: MAEupper", x = "15-Period MAEupper", y= "Correct/Incorrect Ratio",fill="Accuracy")+scale_fill_manual(values=c("#0066FF","#FF4747"))
+ggplot(TestData,aes(x=lEMA15c,fill=TestCorrect))+geom_histogram(binwidth=1,position="fill")+labs(title="Test Accuracy: MAElower", x = "15-Period MAElower", y= "Correct/Incorrect Ratio",fill="Accuracy")+scale_fill_manual(values=c("#0066FF","#FF4747"))
+
+
+Long<-which(Validation$Ratio > 100 & Validation$CCI13 > 100 & Validation$CCI < 225 & Validation$uEMA15c > 9 & Validation$uEMA15c < 17 & Validation$lEMA15c > 15 & Validation$uEMA15c < 22)
+#Isolating the trades
+LongTrades<-Validation[Long,]
+#Creating a dataset of those trades
+LongAcc<-ifelse(LongTrades[,5]=="UP",1,0)
+#Testing the accuracy
+(sum(LongAcc)/length(LongAcc))*100
+#And our long accuracy
+Short<-which(Validation$Ratio < 90 & Validation$CCI13 > -190 & Validation$CCI < -20 & Validation$uEMA15c > -17 & Validation$uEMA15c < 6 & Validation$lEMA15c > -10 & Validation$uEMA15c < 10)
+ShortTrades<-Validation[Short,]
+ShortAcc<-ifelse(ShortTrades[,5]=="DOWN",1,0)
+(sum(ShortAcc)/length(ShortAcc))*100
+#Our short accuracy
+length(LongAcc)+length(ShortAcc)
+#Total number of trades
+((sum(ShortAcc)+sum(LongAcc))/(length(LongAcc)+length(ShortAcc)))*100
+#Total accuracy
